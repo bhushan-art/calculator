affaeba4eb539a66c5c68fb96e9512bd3a63c2a6
Bhushan <bhushandixit6535@gmail.com>
2021-05-24 00:44:24 +0530

    Calculator

---
+++ app/src/main/java/com/example/calculaterapp/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.calculaterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clearonclick(View view) {
+    }
+
+    public void bracnclick(View view) {
+    }
+
+    public void Poweronnclick(View view) {
+    }
+
+    public void divisionnclick(View view) {
+    }
+
+    public void eightonclick(View view) {
+    }
+
+    public void ninenclick(View view) {
+    }
+
+    public void Multipyonclick(View view) {
+    }
+
+    public void sevenOnclick(View view) {
+    }
+
+    public void fourOnclick(View view) {
+    }
+
+    public void fiveOnclick(View view) {
+    }
+
+
+
+    public void sixOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+
+    public void AOnClick(View view) {
+    }
+
+    public void decimaOnClick(View view) {
+    }
+
+    public void eqOnClick(View view) {
+    }
+
+    public void OnClick(View view) {
+    }
+
+    public void zeroOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void subtraOnClick(View view) {
+    }
+
+    public void equalsOnClick(View view) {
+    }
+
+    public void decima1OnClick(View view) {
+    }
+
+    public void decimalOnClick(View view) {
+    }
+
+    public void AdditionOnClick(View view) {
+    }
+
+    public void subtractOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void timesonclick(View view) {
+    }
+
+    public void nineonclick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void divisiononclick(View view) {
+    }
+
+    public void bracketonclick(View view) {
+    }
+}
